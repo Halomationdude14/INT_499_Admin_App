9368b3abc687c30f43f8cc378702575f9097ea4f
Halomationdude14 <79027185+Halomationdude14@users.noreply.github.com>
2024-03-13 18:10:50 -0400

    FINAL CHANGES! Found the proper way to do this finally!

---
+++ INT 499 Application/EZTechMovie_Admin_App.h
@@ -0,0 +1,35 @@
+
+/*
+INT 499 Capstone for Information Technology
+Week 2 - Interactive Assignment
+Date: 2024-03-12
+Author: Paul Oram
+*/
+
+#include <string>
+#include <vector>
+
+#pragma once
+class EZTechMovie_Admin_App {
+	
+	public:
+		EZTechMovie_Admin_App();
+		string strToUpperCase(string);
+		void SetAnswer();
+		void SetMovieTitle();
+		void SetCastSize();
+		void SetCastMembers();
+		void SetRating();
+		void displayMovie();
+		void storeMovie();
+
+	protected:
+		string name;
+		int castSize;
+		vector<string> cast;
+		string rating;
+		char answer;
+		string userInput;
+		vector<string> movieRatings;
+};
+
---
+++ INT 499 Application/main.cpp
@@ -0,0 +1,21 @@
+
+/*
+INT 499 Capstone for Information Technology
+Week 2 - Interactive Assignment
+Date: 2024-03-12
+Author: Paul Oram
+*/ 
+
+#include <iostream>
+using namespace std;
+
+#include "EZTechMovie_Admin_App.h"
+
+
+// Purpose: Main Function
+int main() {
+	EZTechMovie_Admin_App newMovie;
+	cout << "\nThank you for choosing EZTechMovie!" << endl;
+	newMovie.storeMovie();
+	return 0;
+}
